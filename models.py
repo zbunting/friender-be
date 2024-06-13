a33a90d7238f46d1d51fc9baba940a6ff8c84eb7
Zach Bunting <zvrbunting@gmail.com>
2024-06-12 18:28:22 -0700

    Fixed models

--- models.py
+++ models.py
@@ -11,81 +11,6 @@ dbx = db.session.execute
 DEFAULT_IMAGE_NAME = "default.jpg"
 
 
-class User(db.Model):
-    """An individual user"""
-
-    __tablename__ = 'users'
-
-    username = db.mapped_column(
-        db.String(25),
-        primary_key=True
-    )
-
-    hashed_pwd = db.mapped_column(
-        db.String(100),
-        nullable=False,
-    )
-
-    first_name = db.mapped_column(
-        db.String(25),
-        nullable=False,
-    )
-
-    bio = db.mapped_column(
-        db.String(280),
-        nullable=False,
-    )
-
-    image_name = db.mapped_column(
-        db.String(100),
-        nullable=False
-    )
-
-    inc_friends = db.relationship(
-        "Friend",
-        back_populates="is_friended",
-        cascade="all, delete-orphan"
-    )
-
-    out_friends = db.relationship(
-        "Friend",
-        back_populates="is_friending",
-        cascade="all, delete-orphan"
-    )
-
-    inc_likes = db.relationship(
-        "Like",
-        back_populates="is_liked",
-        cascade="all, delete-orphan"
-    )
-
-    out_likes = db.relationship(
-        "Like",
-        back_populates="is_liking",
-        cascade="all, delete-orphan"
-    )
-
-    @classmethod
-    def sign_up(cls, username, password, first_name, bio):
-        """Sign up user.
-
-        Hashes password and adds user to session.
-        """
-
-        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
-
-        user = User(
-            username=username,
-            password=hashed_pwd,
-            first_name=first_name,
-            bio=bio,
-            image=DEFAULT_IMAGE_NAME
-        )
-
-        db.session.add(user)
-        return user
-
-
 class Friend(db.Model):
     """Connection for friends"""
 
@@ -156,3 +81,82 @@ class Like(db.Model):
         foreign_keys=[is_liking_username],
         back_populates="out_likes",
     )
+
+
+class User(db.Model):
+    """An individual user"""
+
+    __tablename__ = 'users'
+
+    username = db.mapped_column(
+        db.String(25),
+        primary_key=True
+    )
+
+    hashed_pwd = db.mapped_column(
+        db.String(100),
+        nullable=False,
+    )
+
+    first_name = db.mapped_column(
+        db.String(25),
+        nullable=False,
+    )
+
+    bio = db.mapped_column(
+        db.String(280),
+        nullable=False,
+    )
+
+    image_name = db.mapped_column(
+        db.String(100),
+        nullable=False
+    )
+
+    inc_friends = db.relationship(
+        "Friend",
+        foreign_keys=[Friend.is_friended_username],
+        back_populates="is_friended",
+        cascade="all, delete-orphan"
+    )
+
+    out_friends = db.relationship(
+        "Friend",
+        foreign_keys=[Friend.is_friending_username],
+        back_populates="is_friending",
+        cascade="all, delete-orphan"
+    )
+
+    inc_likes = db.relationship(
+        "Like",
+        foreign_keys=[Like.is_liked_username],
+        back_populates="is_liked",
+        cascade="all, delete-orphan"
+    )
+
+    out_likes = db.relationship(
+        "Like",
+        foreign_keys=[Like.is_liking_username],
+        back_populates="is_liking",
+        cascade="all, delete-orphan"
+    )
+
+    @classmethod
+    def sign_up(cls, username, password, first_name, bio):
+        """Sign up user.
+
+        Hashes password and adds user to session.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            hashed_pwd=hashed_pwd,
+            first_name=first_name,
+            bio=bio,
+            image_name=DEFAULT_IMAGE_NAME
+        )
+
+        db.session.add(user)
+        return user
